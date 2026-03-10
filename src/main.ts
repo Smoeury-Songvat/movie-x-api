d23b0f1e9d3cd42e46444df91c1cd73b54759d61
Smoeury-Songvat <s.songvat@dgc.gov.kh>
2026-03-10 21:31:29 +0700

    Add swagger api document

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Movie API')
+    .setDescription('Movie API documentation')
+    .setVersion('1.0')
+    .addBearerAuth() // if using JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
